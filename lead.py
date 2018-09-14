477494bb9606cc9b80f888bc26082ce60be11fe7
Yauheni <busko-007@mail.ru>
2018-09-14 21:49:24 +0300

    fsd

--- lead.py
+++ lead.py
@@ -12,7 +12,7 @@ from telebot import types
 import backend
 
 header = {
-    'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"
+    'User-Agent': "Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)"
 }
 
 zag = {'Content-type': 'application/json',  # Определение типа данных
--- servak.py
+++ servak.py
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 from lead import bot
 
 header = {
-    'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"
+    'User-Agent': "Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)"
 }
 
 zag = {'Content-type': 'application/json',  # Определение типа данных
